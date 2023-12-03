8b9d547853a03386f9e52e2c69688aa8a5cc265a
aayushker <singhaayushker@gmail.com>
2023-12-03 20:17:45 +0530

    Completed the search bar

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -1,5 +1,5 @@
 let availableKeywords = [
-    // 'Iphone',
+        // 'Iphone',
     // 'Samsung',
     // 'Oppo',
     // 'Vivo',
@@ -41,21 +41,10 @@ let availableKeywords = [
 
 const resultsBox = document.querySelector(".result-box");
 const inputBox = document.querySelector(".form-control");
-inputBox.onkeyup = function () {
-    let result = [];
-    let input = inputBox.value;
-    if (input.length ) {
-        result = availableKeywords.filter((keyword) => {
-        return keyword.toLowerCase().includes(input.toLowerCase());
-        });
-        console.log(result);
-    }
-    display(result);
-    
-    if(input.length === 0){
-        resultsBox.innerHTML = '';
-    }
-}
+const searchButton = document.getElementById("searchButton");
+
+let currentSelection = -1;
+let keyHoldInterval = null;
 
 function display(result){
     const content = result.map((list) => {
@@ -71,97 +60,101 @@ function selectInput(element){
     resultsBox.style.display = "none";
 }
 
-const searchButton = document.getElementById("searchButton");
-searchButton.addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    let input = inputBox.value;
-    if (input.length) {
-        // Define your logic for determining the URL based on the keyword
-        let url = determineUrl(input);
-        window.location.href = url;
-    }
-});
-
 function determineUrl(keyword) {
     keyword = keyword.toLowerCase();
     if (keyword === "privacy policy") {
-        // If the keyword is "privacy", redirect to the privacy policy page in the subdirectory
         return "/assets/supplements/privacypolicy.html";
-    } 
-    else if (keyword === "terms and conditions") {
-        // If the keyword is "terms", redirect to the terms and conditions page in the subdirectory
+    } else if (keyword === "terms and conditions") {
         return "/assets/supplements/t&c.html";
-    }
-    else {
-        // Otherwise, redirect to the mobile page with the keyword as a hash
+    } else {
         return "/index.html#" + encodeURIComponent(keyword);
     }
 }
 
-inputBox.onkeyup = function () {
+inputBox.onkeyup = function (event) {
+    const { keyCode } = event;
     let result = [];
     let input = inputBox.value;
-    if (input.length ) {
+    if (input.length) {
         result = availableKeywords.filter((keyword) => {
             return keyword.toLowerCase().includes(input.toLowerCase());
         });
-        console.log(result);
-    }
-    display(result);
-
-    if(input.length === 0){
-        resultsBox.innerHTML = '';
-    } else {
-        resultsBox.style.display = ""; // Show the resultsBox when there's input
-    }
-}
-
-// Hide the resultsBox when the user clicks outside of it
-document.addEventListener("click", function(event) {
-    if (event.target !== inputBox) {
-        resultsBox.style.display = "none";
-    }
-});
-
-// Hide the resultsBox when the user clicks on the search button
-searchButton.addEventListener("click", function(event) {
-    resultsBox.style.display = "none";
-});
-
-// Hide the resultsBox when the user presses the Enter key
-inputBox.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        resultsBox.style.display = "none";
-    }
-});
-
-// Hide the resultsBox when the user clicks on a result
-resultsBox.addEventListener("click", function(event) {
-    resultsBox.style.display = "none";
-});
-
-inputBox.onkeyup = function () {
-    let result = [];
-    let input = inputBox.value;
-    if (input.length ) {
-        result = availableKeywords.filter((keyword) => {
-            return keyword.toLowerCase().includes(input.toLowerCase());
-        });
-        console.log(result);
     }
 
     if(result.length === 0){
         resultsBox.innerHTML = '';
-        resultsBox.style.display = "none"; // Hide the resultsBox when there's no match
+        resultsBox.style.display = "none";
     } else {
         display(result);
-        resultsBox.style.display = ""; // Show the resultsBox when there's a match
+        resultsBox.style.display = "";
+    }
+
+    const results = document.querySelectorAll(".result-box li");
+
+    if (keyCode === 38) { // Up arrow
+        event.preventDefault();
+        if (currentSelection > 0) {
+            currentSelection--;
+        }
+    } else if (keyCode === 40) { // Down arrow
+        event.preventDefault();
+        if (currentSelection < results.length - 1) {
+            currentSelection++;
+        }
+    } else if (keyCode === 13) { // Enter key
+        event.preventDefault();
+        if (currentSelection > -1) {
+            let selectedKeyword = results[currentSelection].textContent;
+            inputBox.value = selectedKeyword;
+            resultsBox.style.display = "none";
+            let url = determineUrl(selectedKeyword);
+            window.location.href = url;
+        }
+    } else {
+        // Reset the currentSelection to the first entry when the input changes
+        currentSelection = -1;
+    }
+
+    // Highlight the selected result
+    results.forEach((li, index) => {
+        if (index === currentSelection) {
+            li.style.backgroundColor = "#f0f5ff";
+            li.scrollIntoView({ block: "nearest" });
+        } else {
+            li.style.backgroundColor = "";
+        }
+    });
+
+    // Start an interval to move the selection up or down while the key is held down
+    if (keyCode === 38 || keyCode === 40) {
+        clearInterval(keyHoldInterval);
+        keyHoldInterval = setInterval(() => {
+            if (keyCode === 38 && currentSelection > 0) {
+                currentSelection--;
+            } else if (keyCode === 40 && currentSelection < results.length - 1) {
+                currentSelection++;
+            }
+
+            // Highlight the selected result and scroll it into view
+            results.forEach((li, index) => {
+                if (index === currentSelection) {
+                    li.style.backgroundColor = "#f0f5ff";
+                    li.scrollIntoView({ block: "nearest" });
+                } else {
+                    li.style.backgroundColor = "";
+                }
+            });
+        }, 2); // Change the interval duration to adjust the speed of the selection movement
     }
 }
 
+inputBox.addEventListener("keyup", function(event) {
+    // Clear the interval when the key is released
+    clearInterval(keyHoldInterval);
+});
+
 searchButton.addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+    event.preventDefault();
 
     let input = inputBox.value;
     let result = [];
@@ -172,11 +165,18 @@ searchButton.addEventListener("click", function(event) {
     }
 
     if (result.length === 0) {
-        // Display a "not found" message if there are no matches
         alert("Sorry, the said product is not found.");
     } else {
-        // Redirect to the specific page if there are matches
         let url = determineUrl(input);
         window.location.href = url;
+    }
+
+    resultsBox.style.display = "none";
+});
+
+// Hide the resultsBox when the user clicks outside of it
+document.addEventListener("click", function(event) {
+    if (event.target !== inputBox) {
+        resultsBox.style.display = "none";
     }
 });
