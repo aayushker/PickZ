5674c7c31fc94d6d729ab2ce8e00633343a1761f
aayushker <singhaayushker@gmail.com>
2024-01-18 01:12:44 +0530

    Removed unnecessary comments and resolved the links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   /**
    * Scroll top button
    */
-  let scrollTop = document.querySelector('.scroll-top');
+  let scrollTop = document.querySelector(".scroll-top");
 
   function toggleScrollTop() {
     if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+      window.scrollY > 100
+        ? scrollTop.classList.add("active")
+        : scrollTop.classList.remove("active");
     }
   }
-  scrollTop.addEventListener('click', (e) => {
+  scrollTop.addEventListener("click", (e) => {
     e.preventDefault();
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   });
 
-  window.addEventListener('load', toggleScrollTop);
-  document.addEventListener('scroll', toggleScrollTop);
+  window.addEventListener("load", toggleScrollTop);
+  document.addEventListener("scroll", toggleScrollTop);
 
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
+  const preloader = document.querySelector("#preloader");
   if (preloader) {
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
       preloader.remove();
     });
   }
@@ -35,35 +37,42 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
-  const selectBody = document.querySelector('body');
-  const selectHeader = document.querySelector('#header');
+  const selectBody = document.querySelector("body");
+  const selectHeader = document.querySelector("#header");
 
   function toggleScrolled() {
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    if (
+      !selectHeader.classList.contains("scroll-up-sticky") &&
+      !selectHeader.classList.contains("sticky-top") &&
+      !selectHeader.classList.contains("fixed-top")
+    )
+      return;
+    window.scrollY > 100
+      ? selectBody.classList.add("scrolled")
+      : selectBody.classList.remove("scrolled");
   }
 
-  document.addEventListener('scroll', toggleScrolled);
-  window.addEventListener('load', toggleScrolled);
+  document.addEventListener("scroll", toggleScrolled);
+  window.addEventListener("load", toggleScrolled);
 
   /**
    * Scroll up sticky header to headers with .scroll-up-sticky class
    */
   let lastScrollTop = 0;
-  window.addEventListener('scroll', function() {
-    if (!selectHeader.classList.contains('scroll-up-sticky')) return;
+  window.addEventListener("scroll", function () {
+    if (!selectHeader.classList.contains("scroll-up-sticky")) return;
 
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop && scrollTop > selectHeader.offsetHeight) {
-      selectHeader.style.setProperty('position', 'sticky', 'important');
+      selectHeader.style.setProperty("position", "sticky", "important");
       selectHeader.style.top = `-${header.offsetHeight + 50}px`;
     } else if (scrollTop > selectHeader.offsetHeight) {
-      selectHeader.style.setProperty('position', 'sticky', 'important');
+      selectHeader.style.setProperty("position", "sticky", "important");
       selectHeader.style.top = "0";
     } else {
-      selectHeader.style.removeProperty('top');
-      selectHeader.style.removeProperty('position');
+      selectHeader.style.removeProperty("top");
+      selectHeader.style.removeProperty("position");
     }
     lastScrollTop = scrollTop;
   });
@@ -71,36 +80,35 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Mobile nav toggle
    */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  const mobileNavToggleBtn = document.querySelector(".mobile-nav-toggle");
 
   function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
+    document.querySelector("body").classList.toggle("mobile-nav-active");
+    mobileNavToggleBtn.classList.toggle("bi-list");
+    mobileNavToggleBtn.classList.toggle("bi-x");
   }
-  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+  mobileNavToggleBtn.addEventListener("click", mobileNavToogle);
 
   /**
    * Hide mobile nav on same-page/hash links
    */
-  document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.mobile-nav-active')) {
+  document.querySelectorAll("#navmenu a").forEach((navmenu) => {
+    navmenu.addEventListener("click", () => {
+      if (document.querySelector(".mobile-nav-active")) {
         mobileNavToogle();
       }
     });
-
   });
 
   /**
    * Toggle mobile nav dropdowns
    */
-  document.querySelectorAll('.navmenu .has-dropdown i').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      if (document.querySelector('.mobile-nav-active')) {
+  document.querySelectorAll(".navmenu .has-dropdown i").forEach((navmenu) => {
+    navmenu.addEventListener("click", function (e) {
+      if (document.querySelector(".mobile-nav-active")) {
         e.preventDefault();
-        this.parentNode.classList.toggle('active');
-        this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+        this.parentNode.classList.toggle("active");
+        this.parentNode.nextElementSibling.classList.toggle("dropdown-active");
         e.stopImmediatePropagation();
       }
     });
@@ -109,7 +117,7 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
-  window.addEventListener('load', function(e) {
+  window.addEventListener("load", function (e) {
     if (window.location.hash) {
       if (document.querySelector(window.location.hash)) {
         setTimeout(() => {
@@ -117,7 +125,7 @@ document.addEventListener('DOMContentLoaded', () => {
           let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
           window.scrollTo({
             top: section.offsetTop - parseInt(scrollMarginTop),
-            behavior: 'smooth'
+            behavior: "smooth",
           });
         }, 100);
       }
@@ -128,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
    * Initiate glightbox
    */
   const glightbox = GLightbox({
-    selector: '.glightbox'
+    selector: ".glightbox",
   });
 
   /**
@@ -140,52 +148,68 @@ document.addEventListener('DOMContentLoaded', () => {
    * Init isotope layout and filters
    */
   function initIsotopeLayout() {
-    document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
-      let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-      let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-      let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+    document
+      .querySelectorAll(".isotope-layout")
+      .forEach(function (isotopeItem) {
+        let layout = isotopeItem.getAttribute("data-layout") ?? "masonry";
+        let filter = isotopeItem.getAttribute("data-default-filter") ?? "*";
+        let sort = isotopeItem.getAttribute("data-sort") ?? "original-order";
 
-      let initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-        itemSelector: '.isotope-item',
-        layoutMode: layout,
-        filter: filter,
-        sortBy: sort
-      });
-
-      isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-        filters.addEventListener('click', function() {
-          isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-          this.classList.add('filter-active');
-          initIsotope.arrange({
-            filter: this.getAttribute('data-filter')
-          });
-          if (typeof aosInit === 'function') {
-            aosInit();
+        let initIsotope = new Isotope(
+          isotopeItem.querySelector(".isotope-container"),
+          {
+            itemSelector: ".isotope-item",
+            layoutMode: layout,
+            filter: filter,
+            sortBy: sort,
           }
-        }, false);
-      });
+        );
 
-    });
+        isotopeItem
+          .querySelectorAll(".isotope-filters li")
+          .forEach(function (filters) {
+            filters.addEventListener(
+              "click",
+              function () {
+                isotopeItem
+                  .querySelector(".isotope-filters .filter-active")
+                  .classList.remove("filter-active");
+                this.classList.add("filter-active");
+                initIsotope.arrange({
+                  filter: this.getAttribute("data-filter"),
+                });
+                if (typeof aosInit === "function") {
+                  aosInit();
+                }
+              },
+              false
+            );
+          });
+      });
   }
-  window.addEventListener('load', initIsotopeLayout);
+  window.addEventListener("load", initIsotopeLayout);
 
   /**
    * Init swiper sliders
    */
   function initSwiper() {
-    document.querySelectorAll('.swiper').forEach(function(swiper) {
-      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
+    document.querySelectorAll(".swiper").forEach(function (swiper) {
+      let config = JSON.parse(
+        swiper.querySelector(".swiper-config").innerHTML.trim()
+      );
       new Swiper(swiper, config);
     });
   }
-  window.addEventListener('load', initSwiper);
+  window.addEventListener("load", initSwiper);
 
-    /**
+  /**
    * Frequently Asked Questions Toggle
    */
-    document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
-      faqItem.addEventListener('click', () => {
-        faqItem.parentNode.classList.toggle('faq-active');
+  document
+    .querySelectorAll(".faq-item h3, .faq-item .faq-toggle")
+    .forEach((faqItem) => {
+      faqItem.addEventListener("click", () => {
+        faqItem.parentNode.classList.toggle("faq-active");
       });
     });
 
@@ -195,11 +219,10 @@ document.addEventListener('DOMContentLoaded', () => {
   function aosInit() {
     AOS.init({
       duration: 600,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       once: true,
-      mirror: false
+      mirror: false,
     });
   }
-  window.addEventListener('load', aosInit);
-
+  window.addEventListener("load", aosInit);
 });
